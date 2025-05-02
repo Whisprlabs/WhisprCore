43a22a1a76ea8092cb0fc104c785a3b6cb64fa0a
Whisprlabs <209516062+Whisprlabs@users.noreply.github.com>
2025-05-02 18:00:21 +0200

    Initial commit

---
+++ src/main/AbstractEventEmitter.js
@@ -0,0 +1,44 @@
+class EventEmitter {
+
+    allowedEvents = [];
+    
+    eventRegistry = new Map();
+
+    onEventRegistry(eventName, callback) {
+        if(typeof(eventName) !== "string") {
+            console.warn("Event must be of type string");
+            return;
+        }
+        if(!this.allowedEvents.find((allowedEvent) => allowedEvent === eventName)) {
+            console.warn(`${eventName.toString()} is not a valid event.`);
+            return;
+        }
+        if(typeof(callback) !== "function") {
+            console.warn(`invalid callback function.`);
+            return;
+        }
+
+        if(!this.eventRegistry.has(eventName)) {
+            this.eventRegistry.set(eventName, [callback]);
+        }
+        this.eventRegistry.get(eventName).push(callback);
+    }
+
+    notifyAll(eventName, ...args) {
+        const eventCallbacksExist = this.eventRegistry.has(eventName);
+
+        if(!eventCallbacksExist) {
+            return;
+        }
+
+        for (const cb of this.eventRegistry.get(eventName)) {
+            try {
+                cb(...args);
+            } catch {
+                console.error(`Error in callback: ${cb.name} during event: ${eventName} given arguments: ${args}`)
+            }
+        }
+    }
+}
+
+export { EventEmitter };
---
+++ src/main/XMPPConnection.js
@@ -0,0 +1,147 @@
+import { client, xml } from "@xmpp/client"
+import id from "@xmpp/id";
+import debug from "@xmpp/debug"
+import { EventEmitter } from "./AbstractEventEmitter.js"
+
+class XmppConnectionConfig {
+    constructor({ service, domain, resource, username, password }) {
+        this.service = service;
+        this.domain = domain;
+        this.resource = resource;
+        this.username = username;
+        this.password = password;
+    }
+
+    static builder() {
+        return new XmppConnectionConfigBuilder();
+    }
+}
+
+class XmppConnectionConfigBuilder {
+    constructor() {
+        this.service = null;
+        this.domain = null;
+        this.resource = null;
+        this.username = null;
+        this.password = null;
+        this.debug = false;
+    }
+
+    setService(service) {
+        this.service = service;
+        return this;
+    }
+
+    setDomain(domain) {
+        this.domain = domain;
+        return this;
+    }
+
+    setResource(resource) {
+        this.resource = resource;
+        return this;
+    }
+
+    setUsername(username) {
+        this.username = username;
+        return this;
+    }
+
+    setPassword(password) {
+        this.password = password;
+        return this;
+    }
+    
+    enableDebug() {
+        this.debug = true;
+        return this;
+    }
+
+    validate() {
+        let error = 0;
+    
+        if (!this.service) {
+            error++;
+            console.error("A service must be provided.");
+        }
+        if (!this.domain) {
+            error++;
+            console.error("A domain must be provided.");
+        }
+        if (!this.username) {
+            error++;
+            console.error("A username must be provided.");
+        }
+        if (!this.password) {
+            error++;
+            console.error("A password must be provided.");
+        }
+
+        if(!this.resource) {
+            this.resource = id();
+        }
+    
+        return error === 0;
+    }
+    
+
+    build() {
+        if(!this.validate()) {
+            return undefined;
+        }
+
+        return new XmppConnectionConfig ({
+            service: this.service,
+            domain: this.domain,
+            resource: this.resource,
+            username: this.username,
+            password: this.password,
+        });
+    }
+}
+
+class XmppConnection extends EventEmitter {
+    constructor(connectionConfig) {
+        super();
+        this.allowedEvents = [
+            "error",
+            "offline",
+            "online",
+            "stanza",
+            "connecting",
+            "connect",
+            "opening",
+            "open",
+            "closing",
+            "close",
+            "disconnecting",
+            "disconnect"
+        ];
+
+        if(!(connectionConfig instanceof XmppConnectionConfig)) {
+            throw new Error("Only XmppConnectionConfig can be supplied to XmppConnection.");
+        }
+
+        this.connection = client({
+            service: connectionConfig.service,
+            domain: connectionConfig.domain,
+            resource: connectionConfig.resource,
+            username: connectionConfig.username,
+            password: connectionConfig.password,
+        });
+
+        debug(this.connection, connectionConfig.debug);
+    }
+
+    async start() {
+
+        this.connection.on('stanza', (message) => {
+            this.notifyAll('stanza', message);
+        })
+        await this.connection.start();
+        await this.connection.send(xml("presence"));
+    }
+
+}
+
+export { XmppConnection, XmppConnectionConfig, XmppConnectionConfigBuilder };
